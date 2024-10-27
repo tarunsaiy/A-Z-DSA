d772b8960e7c34a0f56a78ffc141d8389a658ff8
TARUN SAI YENDAVA <143386989+tarunsaiy@users.noreply.github.com>
2024-10-27 16:26:22 +0530

    Delete BasicMath directory

--- BasicMath/CountDigits.java
+++
@@ -1,9 +0,0 @@
-public class CountDigits {
-    public static void main(String[] args) {
-      int n = 4;
-      double noOfDigits = Math.log(n) / Math.log(10);
-      double noOfBinaryDigits = Math.log(n) / Math.log(2);
-      System.out.println((int)noOfDigits + 1);
-      System.out.println((int) noOfBinaryDigits + 1);
-    }
-}
