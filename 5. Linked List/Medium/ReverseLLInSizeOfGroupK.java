e7a27bfd625cef80aec61a3fd0fec3e7bcdedc4d
tarun <saikrishnayendava@gmail.com>
2025-02-25 19:57:34 +0530

    28-02-2025

--- 5. Linked List/Medium/ReverseLLInSizeOfGroupK.java
+++ 5. Linked List/Medium/ReverseLLInSizeOfGroupK.java
@@ -22,7 +22,7 @@ public class ReverseLLInSizeOfGroupK {
                 head = kthnode;
             }
             else {
-                prev.next = temp;
+                prev.next = kthnode;
             }
             prev = temp;
             temp = nextnode;
@@ -44,6 +44,75 @@ public class ReverseLLInSizeOfGroupK {
             temp.next = prev;
             prev = temp;
             temp = nextnode;
+        }
+        return prev;
+    }
+}
+
+//Extended problem
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node
+ {
+ public:
+ int data;
+ Node *next;
+ Node(int data)
+ {
+ this->data = data;
+ this->next = NULL;
+ }
+ };
+
+ *****************************************************************/
+
+public class Solution {
+    public static Node getListAfterReverseOperation(Node head, int n, int b[]) {
+        if (head == null || head.next == null) return head;
+        Node temp = head;
+        Node prev = null;
+        for (int i = 0; i < n && temp != null; i++) {
+            if(b[i] == 0) continue;
+            Node kNode = getKthNode(temp, b[i]);
+            if (kNode == null) {
+                if (prev != null) {
+                    prev.next = kNode;
+                }
+                break;
+            }
+            Node nextnode = kNode.next;
+            kNode.next = null;
+            reverse(temp);
+            if (head == temp) {
+                head = kNode;
+            }
+            else {
+                prev.next = kNode;
+            }
+            prev = temp;
+            temp = nextnode;
+        }
+        if(prev != null) {
+            prev.next = temp;
+        }
+        return head;
+    }
+    static Node getKthNode(Node node, int k){
+        while(node.next != null && k-- > 1){
+            node = node.next;
+        }
+        return node;
+    }
+    static Node reverse(Node head){
+        Node temp = head;
+        Node prev = null;
+        while (temp != null) {
+            Node front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
         }
         return prev;
     }
